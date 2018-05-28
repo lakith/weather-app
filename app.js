c244850ad1dc3f53a27f08195bb26b337747d283
lakith <lakith1995@gmail.com>
2018-05-28 15:42:56 +0530

    fourth commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
  const yargs = require('yargs');
+ const request = require('request');
 
  const geo_code = require('./geo-code/geo-code');
 
@@ -18,7 +19,34 @@ const argv = yargs.options({
 
 var uri = encodeURIComponent(argv.address);
 //console.log(uri);
-geo_code.get_geocode(uri);
+geo_code.get_geocode(uri,(errorCode,result)=>
+    {
+        if(errorCode){
+            console.log(errorCode);
+        } else {
+            console.log (JSON.stringify(result,undefined,2));
+        }
+    });
+
+    console.log("Weather Details");
+
+    request({
+        url:'https://api.darksky.net/forecast/a187c056ec5d2c9e1385cd06b4fe799e/37.8267,-122.4233',
+        json:true
+    },(error,response,body)=>{
+
+        if(error){
+            console.log('something went wrong');
+        } else {
+            console.log("Current weather summery - "+body.currently.summary);
+            console.log("Current temperature - "+body.currently.temperature);
+            console.log("Current humidity - "+body.currently.humidity);
+        }
+
+
+    });
+
 
 
 //"1301 Lombard street philadelphia"
+//https://api.darksky.net/forecast/a187c056ec5d2c9e1385cd06b4fe799e/37.8267,-122.4233
--- geo-code/geo-code.js
+++ geo-code/geo-code.js
@@ -1,6 +1,6 @@
 const request = require('request');
 
-var get_geocode = (uri) =>
+var get_geocode = (uri,callback) =>
 {
     request({
         url :'http://maps.googleapis.com/maps/api/geocode/json?address='+uri
@@ -9,14 +9,21 @@ var get_geocode = (uri) =>
     {
         if(error)
         {
-            console.log('unable to connect to the google server');
+            callback('unable to connect to the google server');
+            //console.log('unable to connect to the google server');
         }else if(body.status === 'ZERO_RESULTS'){
-            console.log('Invalied Adress.');
+            callback('Invalied Adress.');
+            //console.log('Invalied Adress.');
         }else if(body.status === 'OK'){
        //console.log(JSON.stringify(body,undefined,2));
-            console.log(`Address = ${body.results[0].formatted_address}`);
-            console.log(`lat = ${body.results[0].geometry.location.lat}`);
-            console.log(`lat = ${body.results[0].geometry.location.lng}`);
+            callback(undefined, {
+                address:body.results[0].formatted_address,
+                latitude : body.results[0].geometry.location.lat,
+                langitude : body.results[0].geometry.location.lng
+            })
+          //  console.log(`Address = ${body.results[0].formatted_address}`);
+          //  console.log(`lat = ${body.results[0].geometry.location.lat}`);
+          //  console.log(`lat = ${body.results[0].geometry.location.lng}`);
         }else
         {
             console.log('Something went wrong. Try again.');
